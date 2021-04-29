ee2c904e0d4daa5a73e191d72c973923273cdbf6
Diegofigueiredo1 <diegofigueiredo93@gmail.com>
2021-04-29 20:22:32 -0300

    exercicio da semana 12

--- exercicio-semana12/arquivo2.sh
+++ exercicio-semana12/arquivo2.sh
@@ -6,9 +6,12 @@ if [ -f $a ];then
 	echo 'Arquivo passado'
 	cat $a
 	echo 'Arquivo sem quebra de linhas vazias'
-	cat $a | grep . > /tmp/arquivofinal
+	#Deixei essas duas opções!
+	cat $a | grep -v -e '^[[:space:]]*$' > /tmp/arquivofinal
+	#cat $a | grep . > /tmp/arquivofinal
 	cat /tmp/arquivofinal
 else
 	echo "Erro: Arquivo não existe"
 fi
+
 
